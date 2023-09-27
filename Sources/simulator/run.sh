52104707de34868ffeb5a26bb8e313103598d525
Michal Majer <xmajer21@stud.fit.vutbr.cz>
2023-09-27 16:54:06 +0200

    #7: Docker-compose for running both systems

--- Sources/simulator/run.sh
+++ Sources/simulator/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # Usage: ./run.sh [args]
-./gradlew run --args="$@"
+./gradlew run --args="--server --host=0.0.0.0"
---
+++ Sources/stop_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# stop_container.sh
+# Description: Run the docker container (both web server and simulator)
+# Author: Michal Majer
+
+# You may need to run this script with sudo
+
+docker-compose -f "docker-compose.yml" down
