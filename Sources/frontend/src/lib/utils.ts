d956520bfaab22117a35fb258baf164072afab22
Michal Majer <xmajer21@stud.fit.vutbr.cz>
2023-11-10 17:00:24 +0100

    #14:Added configuration for ui.shadcn.com

---
+++ Sources/frontend/src/lib/utils.ts
@@ -0,0 +1,37 @@
+/**
+ * @file    utils.ts
+ *
+ * @author  Michal Majer
+ *          Faculty of Information Technology
+ *          Brno University of Technology
+ *          xmajer21@stud.fit.vutbr.cz
+ *
+ * @brief   Utils for ui components
+ *
+ * @date    10 November 2023, 16:00 (created)
+ *
+ * @section Licence
+ * This file is part of the Superscalar simulator app
+ *
+ * Copyright (C) 2023  Michal Majer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
