be811b26671922d0fb7a96a9ab520bcb1f9b8041
Michal Majer <xmajer21@stud.fit.vutbr.cz>
2023-12-15 14:50:45 +0100

    #17: Documenting the report script

--- Sources/test/loadtesting/Readme.md
+++ Sources/test/loadtesting/Readme.md
@@ -5,3 +5,13 @@
 
 1. Download and install [jmeter](https://jmeter.apache.org/download_jmeter.cgi)
 2. Open the `.jmx` file in jmeter
+
+## Generate report
+
+1. Run the `generate_report.sh` script
+2. Open the `report/index.html` file in your browser
+
+You may need to change the JMETER_PATH variable in the script. For example:
+```bash
+JMETER_PATH="apache-jmeter-5.6.2/bin/jmeter" ./generate_report.sh 
+```
--- Sources/test/loadtesting/generate_report.sh
+++ Sources/test/loadtesting/generate_report.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-JMETER_PATH=apache-jmeter-5.6.2/bin/jmeter
+# This script generates a report from the load testing results.
+
+# Try to find the JMeter path
+# apache-jmeter-5.6.2/bin/jmeter
+JMETER_PATH_DEFAULT=jmeter
+JMETER_PATH="${JMETER_PATH:-$JMETER_PATH_DEFAULT}"
+
+echo "Used JMeter path: $JMETER_PATH"
+
+# Check if JMeter is installed
+if ! [ -x "$(command -v $JMETER_PATH)" ]; then
+  echo "Error: JMeter is not installed or not found." >&2
+  exit 1
+fi
+
+# clear the previous results
+rm -r report
 
 # This script generates a report from the load testing results.
 # -n: non-GUI mode
