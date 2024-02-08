e7c9b19d0a7a8125a47d7096165b691643176ee5
Michal Majer <xmajer21@stud.fit.vutbr.cz>
2024-02-08 10:15:26 +0100

    #13: Server class refactor

--- Sources/simulator/src/main/java/com/gradle/superscalarsim/server/Server.java
+++ Sources/simulator/src/main/java/com/gradle/superscalarsim/server/Server.java
@@ -36,23 +36,32 @@ import io.undertow.Handlers;
 import io.undertow.Undertow;
 import io.undertow.UndertowOptions;
 import io.undertow.server.HttpHandler;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.server.handlers.RequestLimitingHandler;
 import io.undertow.server.handlers.encoding.ContentEncodingRepository;
 import io.undertow.server.handlers.encoding.DeflateEncodingProvider;
 import io.undertow.server.handlers.encoding.EncodingHandler;
 import io.undertow.server.handlers.encoding.GzipEncodingProvider;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 
+import static io.undertow.UndertowOptions.ENABLE_HTTP2;
+
 /**
+ * @details Timeouts long-running requests (simulations stuck in a loop for example).
+ * Rate limits the number of concurrent requests.
  * Supports gzip encoding - client must include "Accept-Encoding: gzip" header.
- *
+ * <a href="https://undertow.io/undertow-docs/undertow-docs-2.1.0/index.html">Docs for Undertow are available here</a>.
  * @class Server
  * @brief Undertow server with handlers for several HTTP endpoints.
  */
 public class Server
 {
-  
+  /**
+   * @brief Host to listen on. Can be configured via command line argument
+   */
   String host;
   
   /**
@@ -64,7 +73,25 @@ public class Server
    */
   int timeout_ms;
   
+  /**
+   * Maximum concurrent requests. If exceeded, the server will queue the requests.
+   */
+  int maxConcurrentRequests = 100;
+  
+  /**
+   * @brief Use gzip encoding (or deflate) for responses
+   */
   boolean useGzip = true;
+  
+  /**
+   * @brief Map of endpoints and their handlers
+   */
+  private final Map<EndpointName, IRequestResolver> endpoints = Map.of(EndpointName.compile, new CompileHandler(),
+                                                                       EndpointName.parseAsm, new ParseAsmHandler(),
+                                                                       EndpointName.checkConfig,
+                                                                       new CheckConfigHandler(), EndpointName.simulate,
+                                                                       new SimulateHandler(), EndpointName.schema,
+                                                                       new SchemaHandler());
   
   public Server(String host, int port, int timeout_ms)
   {
@@ -76,12 +103,9 @@ public class Server
   public void start() throws IOException
   {
     // Register handlers
-    HttpHandler pathHandler = Handlers.path()//
-            .addPrefixPath(EndpointName.compile.getPath(), new MyRequestHandler<>(new CompileHandler()))//
-            .addPrefixPath(EndpointName.parseAsm.getPath(), new MyRequestHandler<>(new ParseAsmHandler()))
-            .addPrefixPath(EndpointName.checkConfig.getPath(), new MyRequestHandler<>(new CheckConfigHandler()))
-            .addPrefixPath(EndpointName.simulate.getPath(), new MyRequestHandler<>(new SimulateHandler()))
-            .addPrefixPath(EndpointName.schema.getPath(), new MyRequestHandler<>(new SchemaHandler()));
+    PathHandler pathHandler = Handlers.path();
+    endpoints.forEach((key, value) -> pathHandler.addPrefixPath(key.getPath(), new MyRequestHandler(value)));
+    
     HttpHandler baseHandler = pathHandler;
     
     // Add gzip encoding
@@ -93,14 +117,15 @@ public class Server
     }
     
     // Add error handling and timeout
+    baseHandler = new RequestLimitingHandler(maxConcurrentRequests, baseHandler);
     baseHandler = new TimeoutHandler(timeout_ms, baseHandler);
     baseHandler = new ErrorHandler(baseHandler);
     
     // Start the server
     Undertow server = Undertow.builder().addHttpListener(port, host) // listen on port
             .setServerOption(UndertowOptions.NO_REQUEST_TIMEOUT,
-                             120 * 1000) // idle connection timeout in milliseconds (!)
-            //.setServerOption(ENABLE_HTTP2, true) // enable HTTP/2
+                             100 * 1000) // idle connection timeout in milliseconds (!)
+            .setServerOption(ENABLE_HTTP2, true) // enable HTTP/2
             .setHandler(baseHandler).build();
     server.start();
     System.out.println("Server running on port " + port);
