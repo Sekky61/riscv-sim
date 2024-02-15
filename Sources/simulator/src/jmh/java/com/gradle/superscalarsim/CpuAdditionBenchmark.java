27725ecf8e3ce787757c00159253d3bd0dd2fd23
Michal Majer <xmajer21@stud.fit.vutbr.cz>
2024-02-16 00:03:26 +0100

    #18: Benchmarks initialization

--- Sources/simulator/src/jmh/java/com/gradle/superscalarsim/CpuAdditionBenchmark.java
+++ Sources/simulator/src/jmh/java/com/gradle/superscalarsim/CpuAdditionBenchmark.java
@@ -25,14 +25,13 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.gradle.superscalarsim.benchmarks;
+package com.gradle.superscalarsim;
 
 import com.gradle.superscalarsim.cpu.Cpu;
 import com.gradle.superscalarsim.cpu.SimulationConfig;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class CpuAdditionBenchmark
 {
@@ -41,46 +40,45 @@ public class CpuAdditionBenchmark
     org.openjdk.jmh.Main.main(args);
   }
   
-  @Fork(value = 1, warmups = 1)
+  /**
+   * Microbenchmark loading of assets
+   */
+  @Fork(value = 1)
+  @Warmup(iterations = 1, time = 1)
   @Benchmark
   @BenchmarkMode(Mode.AverageTime)
-  public Cpu constantSteps()
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public Cpu loadAssets()
   {
-    // 10000 iters on a load/store loop
-    String code = "addi x3 x0 10000\n" + "addi x8 x0 50\n" + "sw x8 x0 16\n" + "loop:\n" + "beq x3 x0 loopEnd\n" + "lw x8 x0 16\n" + "addi x8 x8 1\n" + "sw x8 x0 16\n" + "subi x3 x3 1\n" + "jal x0 loop\n" + "loopEnd:";
-    
-    SimulationConfig config = SimulationConfig.getDefaultConfiguration();
-    config.code = code;
     Cpu cpu = new Cpu();
-    
-    for (int i = 0; i < 100000; i++)
-    {
-      cpu.step();
-    }
-    
     return cpu;
   }
   
-  @Fork(value = 1, warmups = 1)
+  @Fork(value = 1)
+  @Warmup(iterations = 1, time = 1)
   @Benchmark
   @BenchmarkMode(Mode.AverageTime)
-  public Cpu runProgram()
+  @OutputTimeUnit(TimeUnit.MILLISECONDS)
+  public Cpu constantSteps()
   {
-    // 10000 iters on a load/store loop
-    String code = "addi x3 x0 10000\n" + "addi x8 x0 50\n" + "sw x8 x0 16\n" + "loop:\n" + "beq x3 x0 loopEnd\n" + "lw x8 x0 16\n" + "addi x8 x8 1\n" + "sw x8 x0 16\n" + "subi x3 x3 1\n" + "jal x0 loop\n" + "loopEnd:";
+    // 1000 iters on a load/store loop
+    String code = """
+            addi x3, x0, 1000
+            addi x8, x0, 50
+            sw x8, 16(x0)
+            loop:
+            beq x3, x0, loopEnd
+            lw x8, 16(x0)
+            addi x8, x8, 1
+            sw x8, x0, 16
+            subi x3, x3, 1
+            jal x0, loop
+            loopEnd:""";
     
     SimulationConfig config = SimulationConfig.getDefaultConfiguration();
     config.code = code;
-    Cpu cpu = new Cpu();
-    
-    int i = 0;
-    while (!cpu.simEnded())
-    {
-      cpu.step();
-      i++;
-    }
-    
-    System.out.printf("Cycles: %d\n", i);
+    Cpu cpu = new Cpu(config);
+    cpu.execute(false);
     
     return cpu;
   }
