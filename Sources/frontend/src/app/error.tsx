629f8aea5b258210553d48bd03ab36b46adbbcce
Sekky61 <misa@majer.cz>
2024-11-17 13:34:51 +0100

    #27: Show custom error report in all circumstances

--- Sources/frontend/src/app/error.tsx
+++ Sources/frontend/src/app/error.tsx
@@ -1,5 +1,6 @@
 'use client'; // Error boundaries must be Client Components
 
+import ErrorComponent from '@/components/ErrorComponent';
 import { useEffect } from 'react';
 
 export default function Error({
@@ -9,22 +10,5 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    console.error(error);
-  }, [error]);
-
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  );
+  return <ErrorComponent error={error} reset={reset} />;
 }
